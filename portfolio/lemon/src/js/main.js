4a7d54da6a2157972e8b70a79280c066cbb1a6d4
Murgio <muriz.se@gmail.com>
2018-12-20 16:15:04 +0100

    Lemon new landing page

--- portfolio/lemon/src/js/main.js
+++ portfolio/lemon/src/js/main.js
@@ -1,5 +1,4 @@
 (function () {
-  const win = window
   const doc = document.documentElement
 
   doc.classList.remove('no-js')
@@ -10,82 +9,28 @@
     /* global ScrollReveal */
     const sr = window.sr = ScrollReveal()
 
-    sr.reveal('.hero-title, .hero-paragraph, .hero-cta', {
+    sr.reveal('.hero-title, .hero-paragraph, .newsletter-header, .newsletter-form', {
       duration: 1000,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'bottom',
       interval: 150
     })
-  }
 
-  // Moving objects
-  const movingObjects = document.querySelectorAll('.is-moving-object')
+    sr.reveal('.bubble-3, .bubble-4, .hero-browser-inner, .bubble-1, .bubble-2', {
+      duration: 1000,
+      scale: 0.95,
+      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
+      interval: 150
+    })
 
-  // Throttling
-  function throttle (func, milliseconds) {
-    let lastEventTimestamp = null
-    let limit = milliseconds
-
-    return (...args) => {
-      let now = Date.now()
-
-      if (!lastEventTimestamp || now - lastEventTimestamp >= limit) {
-        lastEventTimestamp = now
-        func.apply(this, args)
-      }
-    }
-  }
-
-  // Init vars
-  let mouseX = 0
-  let mouseY = 0
-  let scrollY = 0
-  let coordinateX = 0
-  let coordinateY = 0
-  let winW = doc.clientWidth
-  let winH = doc.clientHeight
-
-  // Move Objects
-  function moveObjects (e, object) {
-    mouseX = e.pageX
-    mouseY = e.pageY
-    scrollY = win.scrollY
-    coordinateX = (winW / 2) - mouseX
-    coordinateY = (winH / 2) - (mouseY - scrollY)
-
-    for (let i = 0; i < object.length; i++) {
-      const translatingFactor = object[i].getAttribute('data-translating-factor') || 20
-      const rotatingFactor = object[i].getAttribute('data-rotating-factor') || 20
-      const perspective = object[i].getAttribute('data-perspective') || 500
-      let tranformProperty = []
-
-      if (object[i].classList.contains('is-translating')) {
-        tranformProperty.push('translate(' + coordinateX / translatingFactor + 'px, ' + coordinateY / translatingFactor + 'px)')
-      }
-
-      if (object[i].classList.contains('is-rotating')) {
-        tranformProperty.push('perspective(' + perspective + 'px) rotateY(' + -coordinateX / rotatingFactor + 'deg) rotateX(' + coordinateY / rotatingFactor + 'deg)')
-      }
-
-      if (object[i].classList.contains('is-translating') || object[i].classList.contains('is-rotating')) {
-        tranformProperty = tranformProperty.join(' ')
-
-        object[i].style.transform = tranformProperty
-        object[i].style.transition = 'transform 1s ease-out'
-        object[i].style.transformStyle = 'preserve-3d'
-        object[i].style.backfaceVisibility = 'hidden'
-      }
-    }
-  }
-
-  // Call function with throttling
-  if (movingObjects) {
-    win.addEventListener('mousemove', throttle(
-      function (e) {
-        moveObjects(e, movingObjects)
-      },
-      150
-    ))
+    sr.reveal('.feature', {
+      duration: 600,
+      distance: '40px',
+      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
+      interval: 100,
+      origin: 'bottom',
+      viewFactor: 0.5
+    })
   }
 }())
